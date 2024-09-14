0b72018d6b14aa313710cd4d654b71ad93745009
Bidhu1024 <bidhu1024av@gmail.com>
2024-09-14 11:31:38 +0530

    Lazy Loading added

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Properties from './pages/Properties/Properties';
-import Navbar from './components/Navbar';
+const Home = React.lazy(()=>import('./pages/Home/Home'))
+const About =React.lazy(()=>import('./pages/About/About'));
+const Properties =React.lazy(()=>import('./pages/Properties/Properties'));
+const Navbar =React.lazy(()=>import( './components/Navbar'));
 
 
 const AppRoutes: React.FC = () => {
   return (
     <Router>
+       <Suspense fallback={<div>Loading...</div>}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/properties" element={<Properties />} />
       </Routes>
+      </Suspense>
     </Router>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,47 @@
-import { Box } from "@mui/material";
-import React from "react";
+import { Box, Typography } from "@mui/material";
+import DummyProperties from "./DummyProperties.json";
+import { memo } from "react";
 
 const Home = () => {
+  console.log(DummyProperties);
   return (
     <Box width="100%">
-      <Box height="20rem" bgcolor={"red"} width={"100%"}></Box>
-      <Box width="100%">
-
+      <Box
+        height="20rem"
+        bgcolor={"red"}
+        width={"100%"}
+        marginTop={".4rem"}
+      ></Box>
+      <Box
+        width="100%"
+        padding={"1rem"}
+        bgcolor={"lightgrey"}
+        sx={{ opacity: "1" }}
+      >
+        <Box
+          display="flex"
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
+          <Typography fontSize={"1.5rem"} color={"black"}>
+            More on For Rent: House & Apartments
+          </Typography>
+          <Typography
+            sx={{
+              textDecoration: "underline",
+              fontWeight: "bold",
+              ":hover": {
+                textDecoration: "none",
+                cursor: "pointer",
+              },
+            }}
+          >
+            View More
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
 };
 
-export default Home;
+export default memo(Home);
